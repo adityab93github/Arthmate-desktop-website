56a7a55a3b11e5e137f18a2be925fcd61578f363
adityab93github <dityab93@gmail.com>
2023-11-10 16:31:05 +0530

    Init app

---
+++ src/screens/Desktop/Desktop.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "./style.css";
+
+export const Desktop = () => {
+  return (
+    <div className="desktop">
+      <div className="div">
+        <p className="next-generation">
+          <span className="text-wrapper">Next generation investment platform</span>
+          <span className="span">&nbsp;</span>
+          <span className="text-wrapper">delivering </span>
+          <span className="span">safe &amp; high returns </span>
+          <span className="text-wrapper">of</span>
+          <span className="span">&nbsp;</span>
+          <span className="text-wrapper-2">up to 13%</span>
+        </p>
+        <div className="overlap-group">
+          <img
+            className="ri-instagram-fill"
+            alt="Ri instagram fill"
+            src="https://c.animaapp.com/7KlcnFAT/img/ri-instagram-fill.svg"
+          />
+          <img
+            className="ic-baseline-facebook"
+            alt="Ic baseline facebook"
+            src="https://c.animaapp.com/7KlcnFAT/img/ic-baseline-facebook.svg"
+          />
+          <img className="bi-linkedin" alt="Bi linkedin" src="https://c.animaapp.com/7KlcnFAT/img/bi-linkedin.svg" />
+          <img
+            className="partner-with-us"
+            alt="Partner with us"
+            src="https://c.animaapp.com/7KlcnFAT/img/partner-with-us.png"
+          />
+          <img className="footer" alt="Footer" src="https://c.animaapp.com/7KlcnFAT/img/footer.png" />
+        </div>
+        <img className="image" alt="Image" src="https://c.animaapp.com/7KlcnFAT/img/image-5@2x.png" />
+        <img className="frame" alt="Frame" src="https://c.animaapp.com/7KlcnFAT/img/frame-16.svg" />
+        <img
+          className="meet-our-leaders"
+          alt="Meet our leaders"
+          src="https://c.animaapp.com/7KlcnFAT/img/meet-our-leaders.png"
+        />
+        <img
+          className="invest-to-get-steady"
+          alt="Invest to get steady"
+          src="https://c.animaapp.com/7KlcnFAT/img/invest-to-get-steady-monthly-earnings@2x.png"
+        />
+        <img
+          className="risk-mitigation"
+          alt="Risk mitigation"
+          src="https://c.animaapp.com/7KlcnFAT/img/risk-mitigation.png"
+        />
+        <img className="plans" alt="Plans" src="https://c.animaapp.com/7KlcnFAT/img/plans.png" />
+        <img className="testimonials" alt="Testimonials" src="https://c.animaapp.com/7KlcnFAT/img/testimonials.png" />
+      </div>
+    </div>
+  );
+};
